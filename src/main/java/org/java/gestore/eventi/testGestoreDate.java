11eeac629f7ffbdcf46d795c654746d173041248
victor bruschelli <165551238+VictorBruschelli@users.noreply.github.com>
2024-07-26 22:52:03 +0200

    risolto metodo per comparare date
    *dava errore per non aver dichiarato formatter in DateTimeFormatter.*
    
    consultato la documentazione java per  DateTimeFormatter cosi potendo passare il valore di LocalDate in String per riuscire a comparare le date con il metodo parse().

--- src/main/java/org/java/gestore/eventi/testGestoreDate.java
+++ src/main/java/org/java/gestore/eventi/testGestoreDate.java
@@ -3,7 +3,7 @@ package org.java.gestore.eventi;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Scanner;
 
@@ -17,26 +17,33 @@ public class testGestoreDate {
 		String dataEvento = scanner.nextLine();
 		//
 		
-		Instant realDate;
-		System.out.println(realDate + " questa è la data attuale ");
+		//LocalDate realDate = LocalDate.now();
+		//System.out.println(realDate + " questa è la data attuale ");
+		  LocalDate realDate = LocalDate.now();
+		  DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		  String text = realDate.format(formatter);
+		//  LocalDate parsedDate = LocalDate.parse(text, formatter);
+		  
+		 // System.out.println(realDate);
+		 
+		
 		
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
         Date date1 = sdf.parse(dataEvento);
-        Date date2 = sdf.parse(realDate);
-        sdf.format(realDate);
+        Date date2 = sdf.parse(text);
 
-        System.out.println("date1 : " + sdf.format(date1));
-        System.out.println("date2 : " + sdf.format(date2));
+        System.out.println("date1 : " + sdf.format(date1) + " data inserita dall utente");
+        System.out.println("date2 : " + sdf.format(date2) + " data attuale");
 
         int result = date1.compareTo(date2);
         System.out.println("result: " + result);
 
         if (result == 0) {
-            System.out.println("Date1 is equal to Date2");
+            System.out.println("Date1 è uguale a  Date2");
         } else if (result > 0) {
-            System.out.println("Date1 is after Date2");
+            System.out.println("Date1 è dopo Date2");
         } else if (result < 0) {
-            System.out.println("Date1 is before Date2");
+            System.out.println("Date1 è prima Date2");
         } else {
             System.out.println("How to get here?");
         }
